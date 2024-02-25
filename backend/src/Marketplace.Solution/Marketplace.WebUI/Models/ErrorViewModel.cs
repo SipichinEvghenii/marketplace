d01cf6626c3a823df52e1c33a9d2abc2fef6b292
Tureac Vadim <widths-gerund-0x@icloud.com>
2024-02-25 21:23:30 +0300

    Add .NET Framework support

--- backend/src/Marketplace.Solution/Marketplace.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Marketplace.API.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
