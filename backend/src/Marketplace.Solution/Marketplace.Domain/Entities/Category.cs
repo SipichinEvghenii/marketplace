4ae1bcba0600a20530d8e3bbb0f6507ef2bf63d0
Sipichin Evghenii <pinky.normal.0n@icloud.com>
2024-03-03 21:13:03 +0200

    Add some models

---
+++ backend/src/Marketplace.Solution/Marketplace.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Marketplace.Domain.Entities
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        // Parent Category Relationship
+        public string ParentCategoryId { get; set; }
+        public Category ParentCategory { get; set; }
+
+        // Navigation Property for Subcategories and Products
+        public ICollection<Category> SubCategories { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
