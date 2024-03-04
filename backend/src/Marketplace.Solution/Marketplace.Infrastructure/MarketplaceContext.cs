987feb333d64821848901bc0e1b52e88fd51eaef
Sipichin Evghenii <pinky.normal.0n@icloud.com>
2024-03-04 16:17:55 +0200

    Add basic db context

---
+++ backend/src/Marketplace.Solution/Marketplace.Infrastructure/MarketplaceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using Marketplace.Domain.Entities;
+
+public class MarketplaceContext : DbContext
+{
+    public MarketplaceContext()
+        : base("name=MarketplaceContext") // Specify the name of the connection string
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+}
