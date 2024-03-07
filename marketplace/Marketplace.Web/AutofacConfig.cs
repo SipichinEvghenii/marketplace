6c575f3b7270741a2d3fc4e3e7da22c8d58d77f4
Ciorba Denis <sagas.spill-09@icloud.com>
2024-03-07 13:08:02 +0200

    Add Autofac support

---
+++ marketplace/Marketplace.Web/AutofacConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Marketplace.Application.Interfaces;
+using Marketplace.Application.Services;
+using Marketplace.Infrastructure.Repositories;
+
+namespace Marketplace.Web
+{
+    public class AutofacConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            // Регистрация контроллеров в MVC
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Регистрация сервисов и репозиториев
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerRequest();
+            builder.RegisterType<PasswordHasher>().As<IPasswordHasher>().InstancePerRequest();
+            builder.RegisterType<AuthService>().As<IAuthService>().InstancePerRequest();
+            builder.RegisterType<MarketplaceContext>().InstancePerRequest();
+
+            // Установка резолвера зависимостей
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ marketplace/Marketplace.Web/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace Marketplace.Web
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutofacConfig.Configure(); // Инициализация Autofac
+        }
+        
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+            {
+                var cookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                var ticket = FormsAuthentication.Decrypt(cookie.Value);
+                var identity = new FormsIdentity(ticket);
+                var principal = new GenericPrincipal(identity, null);
+                HttpContext.Current.User = principal;
+            }
+        }
+    }
+}
