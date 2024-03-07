1a707d73766f1a637add7591e20ef11a97b41577
Sipichin Evghenii <pinky.normal.0n@icloud.com>
2024-03-07 17:00:43 +0200

    Add view models

---
+++ marketplace/Marketplace.Domain/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Marketplace.Domain.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
